9ca845f601a105ff12c8ffc3f9f9b5ab88745cea
infodoug <douglasrafael.info@gmail.com>
2024-10-13 20:30:57 -0300

    animação titulo login

--- script.js
+++ script.js
@@ -6,3 +6,28 @@
         pathInput.value = path; // Define o valor do campo oculto com o caminho do arquivo
         document.getElementById('formUpload').submit(); // Submete o formulário após definir o caminho do arquivo
     }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const title = "Millenium";
+        const h1 = document.querySelector("h1");
+        h1.innerHTML = ""; // Limpa o conteúdo inicial
+        let index = 0;
+    
+        function typeLetter() {
+            if (index < title.length) {
+                h1.innerHTML += title[index]; // Adiciona a letra ao h1
+                index++;
+                setTimeout(typeLetter, 150); // Ajuste o tempo para a velocidade da animação
+                h1.style.opacity = 1;
+                h1.style.animation = 0;
+            } else {
+                h1.style.opacity = 1; // Torna o título visível após completar a digitação
+                h1.style.animation = "colorChange 10s infinite";
+            }
+        }
+    
+        // Começa a animação
+        typeLetter();
+    });
+    
+    
