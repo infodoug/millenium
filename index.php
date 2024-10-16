fecfa0e429c6ecdb8b36e0976ff9849e48704f99
infodoug <douglasrafael.info@gmail.com>
2024-10-15 23:03:26 -0300

    mudando card central

--- index.php
+++ index.php
@@ -11,16 +11,14 @@
   <div class="container">
     <form action="testLogin.php" method="POST">
       <div class="form-fields">
-        <label for="email">E-mail:</label>
-        <input type="text" name="email" required>
+        <input type="text" placeholder ="email" name="email" required>
       </div>
       <div class="form-fields">
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" required>
+        <input type="password" placeholder="senha" name="senha" required>
       </div>
       <input class="inputSubmit" type="submit" name="submit" value="Entrar">
       <p class="forgot-password"><a href="#">Esqueceu a senha? Clique aqui</a></p>
-      <p class="nova-conta">Ainda não tem conta?<a href="criar-conta.php">Crie uma conta!</a></p>
+      <p class="nova-conta">Ainda não tem conta? <a href="criar-conta.php">Cadastre-se!</a></p>
     </form>
   </div>
 
