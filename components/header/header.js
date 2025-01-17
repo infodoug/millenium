a9a6202b119191afc37fe2f1ecb3d72805d4b1bf
infodoug <douglasrafael.info@gmail.com>
2025-01-17 14:22:27 -0300

    animation configs

--- components/header/header.js
+++ components/header/header.js
@@ -1,7 +1,16 @@
 const configOptions = document.querySelector('.config-options');
 const configButton = document.querySelector('.config-button');
+const configImg = document.querySelector('.img-config');
 
 configButton.addEventListener('click', function() {
     console.log("aaaaaaaaaa");
     configOptions.classList.toggle('active');
+    configImg.classList.toggle('rotated');
 });
+
+
+
+/* configButton.addEventListener('click', function() {
+    console.log("aaaaaaaaaa");
+    configOptions.classList.toggle('active');
+}); */
