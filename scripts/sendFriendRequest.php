6b9cb795b1d3b73574b10e3833cbd8632c555ca5
infodoug <douglasrafael.info@gmail.com>
2025-12-03 19:20:50 -0300

    feat(friend request): SQL send & unsend friend request

--- scripts/sendFriendRequest.php
+++ scripts/sendFriendRequest.php
@@ -1,17 +1,38 @@
 <?php
 session_start();
-include('../config.php'); // Ajuste o caminho para conectar no banco
+include('../config.php');
 
 if(isset($_POST['id_solicitante']) && isset($_POST['id_solicitado'])){
     
-    $id_solicitante = mysqli_real_escape_string($conexao, $_POST['id_solicitante']);
-    $id_solicitado = mysqli_real_escape_string($conexao, $_POST['id_solicitado']);
+    $id_user = mysqli_real_escape_string($conexao, $_POST['id_solicitante']);
+    $id_friend = mysqli_real_escape_string($conexao, $_POST['id_solicitado']);
 
-    // Verifica se já não são amigos ou se já tem solicitação (Opcional, mas recomendado)
-    // ... lógica de verificação aqui ...
 
-    $sql = "INSERT INTO friends (id_solicitante, id_solicitado) VALUES ('$id_solicitante', '$id_solicitado')";
+    // if !isFriend && not friend request found in friends table {
+    //$sql = "INSERT INTO friends (id_solicitante, id_solicitado) VALUES ('$id_solicitante', '$id_solicitado')";
+
+    $request_sent = "SELECT * FROM friends
+                            WHERE (id_solicitante = '$id_user' AND id_solicitado = '$id_friend')
+                            OR (id_solicitante = '$id_friend' AND id_solicitado = '$id_user')";
+
+    $check_request_sent = $conexao->query($request_sent);
+
+    if ($check_request_sent->num_rows == 0) {
+        $sql = "INSERT INTO friends (id_solicitante, id_solicitado) VALUES ('$id_user', '$id_friend')";
+    } else {
+        $sql = "DELETE FROM friends 
+                    WHERE (id_solicitante = '$id_user' AND id_solicitado = '$id_friend') 
+                    OR    (id_solicitante = '$id_friend' AND id_solicitado = '$id_user')";
+    }
     
+
+
+    
+    // else if !isFriend && friend request found in friends table {
+    // sql DELETE row WHERE friend request found
+
+
+
     if($conexao->query($sql) === TRUE) {
         echo "sucesso";
     } else {
